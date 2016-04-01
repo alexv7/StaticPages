6a7e0c8c8375e53796205b58091652c7efd03e61
alexv7 <alexvarjabedian7@gmail.com>
2016-03-31 22:17:43 -0400

    destroy method for micropost done. Fully functional create and destroy of microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,7 +14,7 @@ class MicropostsController < ApplicationController
     end
   end
 
-# At this point, creating a new micropost works as expected, as seen in 
+# At this point, creating a new micropost works as expected, as seen in
 # Figure 11.15. There is one subtlety, though: on failed micropost submission,
 # the Home page expects an @feed_items instance variable, so failed submissions
 # currently break. The easiest solution is to suppress the feed entirely by
@@ -21,12 +23,36 @@ class MicropostsController < ApplicationController
 # on a pagination link to see why.)
 
 
-  def destroy
-  end
+ def destroy
+   @micropost.destroy
+   flash[:success] = "Micropost deleted"
+   redirect_to request.referrer || root_url
+ end
+
+# This uses the request.referrer method,11 which is closely related to the 
+# request.url variable used in friendly forwarding (Section 9.2.3), and is just
+# the previous URL (in this case, the Home page).12 This is convenient because
+# microposts appear on both the Home page and on the user’s profile page, so by
+# using request.referrer we arrange to redirect back to the page issuing the
+# delete request in both cases. If the referring URL is nil (as is the case inside
+# some tests), Listing 11.50 sets the root_url as the default using the || operator.
+
+
+
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
     end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id]) #this is a way to check that the current user is the owner of that micropost that will be deleted
+      redirect_to root_url if @micropost.nil?
+    end
+
+    # We’ll find the micropost through the association, which will automatically fail
+    # if a user tries to delete another user’s micropost. We’ll put the resulting find
+    # inside a correct_user before filter, which checks that the current user actually
+    # has a micropost with the given id.
 end
