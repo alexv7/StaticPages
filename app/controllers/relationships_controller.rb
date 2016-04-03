011803bff10a2ef16869c78881b45091f352aac8
alexv7 <alexvarjabedian7@gmail.com>
2016-04-02 22:17:05 -0400

    added notes in relationships controller for ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -37,4 +37,11 @@ class RelationshipsController < ApplicationController
   # fine in browsers that have JavaScript disabled (although a small amount of
   # configuration is necessary, as shown in last line of config/application.rb
 
+  # In the case of an Ajax request, Rails automatically calls a JavaScript 
+  # embedded Ruby (.js.erb) file with the same name as the action, i.e., create.js.erb
+  # or destroy.js.erb. As you might guess, such files allow us to mix JavaScript
+  # and embedded Ruby to perform actions on the current page. It is these files
+  # that we need to create and edit in order to update the user profile page upon
+  # being followed or unfollowed.
+
 end
